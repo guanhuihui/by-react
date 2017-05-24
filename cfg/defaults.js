6b49ef10af27501dea512bc6f1ba602fb25cd4fc
omyrobin <omyrobin@163.com>
2017-05-24 17:40:47 +0800

    1111111

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -34,7 +34,7 @@ function getDefaultModules() {
       },
       {
         test: /\.scss/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+        loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded'
       },
       {
         test: /\.less/,
